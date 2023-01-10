84e4bbee7e02e644533b2c6b8f059722234d6327
Duvantrick <duvanli@hotmail.es>
2023-01-10 16:32:26 +1000

    try deploy 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,40 @@
 import React, { useState } from 'react';
 import { Layout } from '../components/layouts';
 import { Navbar, SideBar } from '../components/UI';
-// import { DataResponse } from '../interfaces';
-// import { GetServerSideProps } from 'next';
-// import { clienteAxios } from '../config';
+import { DataResponse } from '../interfaces';
+import { GetServerSideProps } from 'next';
+import { clienteAxios } from '../config';
 import { TabsComponent } from '../components/UI/TabsComponent';
 
-// interface Props {
-//   data: DataResponse;
-// }
+interface Props {
+  data: DataResponse;
+}
 
-const Home = () => {
+const Home = ({ data }: Props) => {
   const [openMenu, setOpenMenu] = useState<boolean>(false);
 
   return (
     <Layout title="Test orange">
       <>
-        <Navbar title={'asd'} setOpenMenu={setOpenMenu} />
+        <Navbar
+          title={data ? data.data[0].namePage : ''}
+          setOpenMenu={setOpenMenu}
+        />
         <TabsComponent />
-        <SideBar openMenu={openMenu} setOpenMenu={setOpenMenu} />
+        <SideBar openMenu={openMenu} setOpenMenu={setOpenMenu} data={data} />
       </>
     </Layout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data } = await clienteAxios.get<DataResponse>('/information');
+
+  return {
+    props: {
+      data: data,
+    },
+  };
 };
 
 export default Home;
