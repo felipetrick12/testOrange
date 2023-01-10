3cbacd009e2a2e09e083ab60ba73abc123953cb3
Duvantrick <duvanli@hotmail.es>
2023-01-10 16:30:09 +1000

    try deploy

--- components/UI/SideBar.tsx
+++ components/UI/SideBar.tsx
@@ -3,22 +3,22 @@ import {
   Typography,
   Box,
   List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
+  // ListItem,
+  // ListItemIcon,
+  // ListItemText,
   Divider,
 } from '@mui/material';
 import React from 'react';
-import InboxIcon from '@mui/icons-material/Inbox';
-import MailIcon from '@mui/icons-material/Mail';
+// import InboxIcon from '@mui/icons-material/Inbox';
+// import MailIcon from '@mui/icons-material/Mail';
 import { DataResponse } from '../../interfaces';
 
 interface Props {
   openMenu: boolean;
   setOpenMenu: (arg0: boolean) => void;
-  data: DataResponse;
+  data?: DataResponse;
 }
-export const SideBar = ({ openMenu, setOpenMenu, data }: Props) => {
+export const SideBar = ({ openMenu, setOpenMenu }: Props) => {
   return (
     <Drawer anchor="left" open={openMenu} onClose={() => setOpenMenu(false)}>
       <Box width={250}>
@@ -31,14 +31,14 @@ export const SideBar = ({ openMenu, setOpenMenu, data }: Props) => {
         <Divider />
 
         <List>
-          {data.data.map((data, index) => (
+          {/* {data.data.map((data, index) => (
             <ListItem key={index}>
               <ListItemIcon>
                 {index % 2 ? <InboxIcon /> : <MailIcon />}
               </ListItemIcon>
               <ListItemText primary={data.namePage} />
             </ListItem>
-          ))}
+          ))} */}
         </List>
         <Divider />
       </Box>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,27 @@
 import React, { useState } from 'react';
 import { Layout } from '../components/layouts';
 import { Navbar, SideBar } from '../components/UI';
-import { DataResponse } from '../interfaces';
-import { GetServerSideProps } from 'next';
-import { clienteAxios } from '../config';
+// import { DataResponse } from '../interfaces';
+// import { GetServerSideProps } from 'next';
+// import { clienteAxios } from '../config';
 import { TabsComponent } from '../components/UI/TabsComponent';
 
-interface Props {
-  data: DataResponse;
-}
+// interface Props {
+//   data: DataResponse;
+// }
 
-const Home = ({ data }: Props) => {
+const Home = () => {
   const [openMenu, setOpenMenu] = useState<boolean>(false);
 
   return (
     <Layout title="Test orange">
       <>
-        <Navbar title={data.data[0].namePage} setOpenMenu={setOpenMenu} />
+        <Navbar title={'asd'} setOpenMenu={setOpenMenu} />
         <TabsComponent />
-        <SideBar openMenu={openMenu} setOpenMenu={setOpenMenu} data={data} />
+        <SideBar openMenu={openMenu} setOpenMenu={setOpenMenu} />
       </>
     </Layout>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const { data } = await clienteAxios.get<DataResponse>('/information');
-
-  return {
-    props: {
-      data: data,
-    },
-  };
 };
 
 export default Home;
